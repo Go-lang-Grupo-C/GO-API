0c432bda7c9c5b43aae42acc9b08a01a0c553bed
taglyscostacurta <taghcc@hotmail.com>
2022-12-05 10:50:30 -0300

    Validator Product

--- models/models.go
+++ models/models.go
@@ -1,14 +1,22 @@
 package models
 
 import (
+	"gopkg.in/validator.v2"
 	"gorm.io/gorm"
 )
 
 type Product struct {
 	gorm.Model        // o gorm ja traz o id unico e o created_at e o updated_at
-	Name       string `json:"name"`
-	Price      uint   `json:"price"`
-	Code       string `json:"code"`
+	Name       string `json:"name" validate:"nonzero"`
+	Price      uint   `json:"price" validate:"nonzero"`
+	Code       string `json:"code" validate:"nonzero"`
 }
 
 var Products []Product
+
+func Validade(product *Product) error {
+	if err := validator.Validate(product); err != nil {
+		return err
+	}
+	return nil
+}
