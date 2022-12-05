82991fa90f4fe5562948c92c00fcca1f6f282837
felipedorizo <105953634+felipedorizo@users.noreply.github.com>
2022-12-05 12:14:52 -0300

    update config

---
+++ config/config.go
@@ -0,0 +1,20 @@
+package config
+
+//config server
+const (
+	//Port is the port that the server will run on
+	PortServer = "8080"
+)
+
+//config database
+const (
+	StringConectDatabase = "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+)
+
+//config routes
+const (
+	//api name
+	ApiName = "api/v1"
+	//products
+	Products = "products"
+)
