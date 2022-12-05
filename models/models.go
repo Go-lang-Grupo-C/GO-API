4f48ce915d2ac9217b498054920bdb69f8954cc6
taglyscostacurta <taghcc@hotmail.com>
2022-12-05 10:16:11 -0300

    Create Models and install gin

--- models/models.go
+++ models/models.go
@@ -5,8 +5,10 @@ import (
 )
 
 type Product struct {
-	gorm.Model
-	Name  string
-	Price uint
-	Code  string
+	gorm.Model        // o gorm ja traz o id unico e o created_at e o updated_at
+	Name       string `json:"name"`
+	Price      uint   `json:"price"`
+	Code       string `json:"code"`
 }
+
+var Products []Product
