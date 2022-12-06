753493c6b0520af46df84f42dad2c7da5fcdf18d
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-12-06 11:13:17 -0300

    documentado :D

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// função que lista todos os produtos
 func Products(c *gin.Context) {
 	var products []models.Product
 	//models.DB.Find(&products)
@@ -12,33 +13,41 @@ func Products(c *gin.Context) {
 
 }
 
+// função que busca somente um produto por ID via paramentro
 func SearchForProduct(c *gin.Context) {
 	var product models.Product
 	//id := c.Params.ByName("id")
 	//database.DB.First(&product, id)
+
+	//valido se o prodtuo existe caso ele for id 0 retorna um erro
 	if product.ID == 0 {
 		c.JSON(404, gin.H{"message": "Product not found"})
 		return
 	}
 	c.JSON(200, gin.H{
-
+		//me tras apenas codigo, nome e preço
 		"code:":  product.Code,
 		"name:":  product.Name,
 		"price:": product.Price,
 	})
 }
 
+// criando novo produto
 func CreateProduct(c *gin.Context) {
 	var product models.Product
+	//valido se o produto foi preenchido corretamente via corpo da requisição JSON
 	if err := c.ShouldBindJSON(&product); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
+	//chamo o metodo de validação cirado no models
 	if err := models.Validate(&product); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 	//database.DB.Create(&product)
+
+	//retorno o produto criado
 	c.JSON(200, gin.H{
 		"ID":    product.ID,
 		"code":  product.Code,
@@ -48,23 +57,30 @@ func CreateProduct(c *gin.Context) {
 
 }
 
+// editar o produto pego ele via paramentro pelo ID
 func UpdateProduct(c *gin.Context) {
 	var product models.Product
 	//id := c.Params.ByName("id")
 	//database.DB.First(&product, id)
+
+	//valido se o produto existe
 	if product.ID == 0 {
 		c.JSON(404, gin.H{"message": "Product not found"})
 		return
 	}
+	//valido se o produto foi preenchido corretamente via corpo da requisição JSON
 	if err := c.ShouldBindJSON(&product); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
+	//chamo o metodo de validação cirado no models
 	if err := models.Validate(&product); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 	//database.DB.Save(&product)
+
+	//retorno o mensagem de sucesso
 	c.JSON(200, gin.H{"message": "Product updated is successfully"})
 }
 
@@ -72,10 +88,14 @@ func DeleteProduct(c *gin.Context) {
 	var product models.Product
 	//id := c.Params.ByName("id")
 	//database.DB.First(&product, id)
+
+	//valido se o produto existe
 	if product.ID == 0 {
 		c.JSON(404, gin.H{"message": "Product not found"})
 		return
 	}
 	//database.DB.Delete(&product)
+
+	//retorno o mensagem de sucesso
 	c.JSON(200, gin.H{"message": "Product deleted is successfully"})
 }
