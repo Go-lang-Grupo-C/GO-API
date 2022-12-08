2d6b9db2238585a1dc5deda60387432e95a1dc47
felipedorizo <105953634+felipedorizo@users.noreply.github.com>
2022-12-08 14:51:53 -0300

    aminnova

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/Go-lang-Grupo-C/GO-API/database"
 	"github.com/Go-lang-Grupo-C/GO-API/server"
 )
@@ -14,4 +17,15 @@ func main() {
 
 	// start server
 	sever.Start()
+}
+
+func handleArticles(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	js, err := json.Marshal(Articles)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
