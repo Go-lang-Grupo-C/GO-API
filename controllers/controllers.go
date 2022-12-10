212423001d8ca332dad2e230f84971560e75254a
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-12-10 10:13:30 -0300

    Testa ai taglyes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -18,7 +18,7 @@ func Products(c *gin.Context) {
 }
 
 // função que busca somente um produto por ID via paramentro
-func SearchForProduct(c *gin.Context) {
+func jSearchForProduct(c *gin.Context) {
 
 	var product models.Product
 	id := c.Params.ByName("ID")
